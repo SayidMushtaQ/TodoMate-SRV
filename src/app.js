c0f3e9e91de7f3197425b8e2d2afe6968009a164
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-09 10:15:31 +0530

    Prettier - Init

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import { DATA_LIMIT } from "./constants.js";
-import cookie_parser from 'cookie-parser';
+import cookie_parser from "cookie-parser";
 const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  })
+);
 
 //Down below: Data configaration
-app.use(express.json({limit:DATA_LIMIT}));
-app.use(express.urlencoded({extended:true,limit:DATA_LIMIT}));
-app.use(express.static('public'));
+app.use(express.json({ limit: DATA_LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: DATA_LIMIT }));
+app.use(express.static("public"));
 app.use(cookie_parser(process.env.COOKIE_PARSER_SECRET));
 
 export { app };
