4cf361728a67b1540fe1bc83bac880d3ded97a97
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-11 09:37:46 +0530

    Password hash Hook and Comapre method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,11 @@
 import {asyncHanlder} from '../util/asyncHandler.js';
 import {ApiResponse} from '../util/apiResponse.js';
-export const registerUser = asyncHanlder((req,res)=>{
+import {User} from '../modules/user.model.js';
+export const registerUser = asyncHanlder(async(req,res)=>{
+    console.log(req.body);
+    // const registerUser = await User.create({
+
+    // })
     res.status(200).json(
         new ApiResponse(200,{ok:'Ok'},'Data received sucessfully')
     )
--- src/modules/user.model.js
+++ src/modules/user.model.js
@@ -1,31 +1,42 @@
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
     userName: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
+      lowercase: true
     },
     email: {
       type: String,
       required: true,
-      unique: true,
+      unique: true
     },
     password: {
       type: Number,
       required: true,
-      min: [6, 'At least set 6 character password'],
+      min: [6, "At least set 6 character password"]
     },
     phone: {
       type: Number,
-      required: true,
+      required: true
     },
     isVerified: Boolean,
-    isActive: Boolean,
+    isActive: Boolean
   },
   { timestamps: true }
 );
 
-export const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcryptjs.hash(this.password, 10);
+  next();
+});
+
+userSchema.method.isPasswordCorrect = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
