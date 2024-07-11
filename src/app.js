2188defa1d35c8b5f11e6a4f474fc4502909537f
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-11 17:10:01 +0530

    URL V. control - IMP

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { DATA_LIMIT } from "./constants.js";
+import { DATA_LIMIT,API_VERSION_URL } from "./constants.js";
 import cookie_parser from "cookie-parser";
 import todoRouter from './routes/todo.router.js';
 const app = express();
@@ -20,7 +20,7 @@ app.use(cookie_parser(process.env.COOKIE_PARSER_SECRET));
 
 //Import routes
 import userRouter from './routes/user.router.js';
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/todo',todoRouter);
+app.use(`${API_VERSION_URL}/users`,userRouter);
+app.use(`${API_VERSION_URL}/todo`,todoRouter);
 
 export { app };
