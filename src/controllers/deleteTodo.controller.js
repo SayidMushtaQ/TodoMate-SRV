0c4aea02e673bbe846edd17e37caec4be316893f
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-12 09:56:55 +0530

    Delete Todo - CRUD(D)

---
+++ src/controllers/deleteTodo.controller.js
@@ -0,0 +1,14 @@
+import { ApiResponse } from "../util/apiResponse.js";
+import { asyncHanlder } from "../util/asyncHandler.js";
+import {Todo} from '../modules/todo.model.js';
+import {ApiError} from '../util/apiError.js';
+
+export const deleteTodo = asyncHanlder(async(req,res)=>{
+    const {todoID} = req.body;
+    const todo = await Todo.findById({_id:todoID}).select('_id');
+    if(!todo){
+        throw new ApiError(404,'The requested Todo item was not found.')
+    }
+    const deletedTodo = await Todo.findByIdAndDelete({_id:todoID});
+    res.status(204).send()
+})
--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import { createTodo } from '../controllers/createTodo.controller.js';
 import {readTodos} from '../controllers/readTodos.controller.js';
 import { updateTodo } from '../controllers/updateTodo.controller.js';
+import { deleteTodo } from '../controllers/deleteTodo.controller.js';
 
 const router = Router();
 
@@ -9,5 +10,6 @@ const router = Router();
 router.route('/createTodo').post(createTodo);
 router.route('/todos').get(readTodos);
 router.route('/updateTodo').patch(updateTodo);
+router.route('/deleteTodo').delete(deleteTodo);
 
 export default router;
