da8875d3d6072f487119a066d018a35e215f42d4
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-12 11:24:19 +0530

    User login and register

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,10 @@
-import {registerUser} from '../controllers/user.controller.js'
+import {registerUser} from '../controllers/userRegister.controller.js'
+import {userLogin} from '../controllers/userLogin.controller.js';
 import {Router} from 'express'
 
 const router = Router();
 
 router.route('/register').post(registerUser);
+router.route('/login').post(userLogin);
 
 export default router;
