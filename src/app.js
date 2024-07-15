b2ad0b9fbadee832e7c6fd4bcef2996cfcc190fd
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-15 18:24:33 +0530

    IMP Names and Import

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { DATA_LIMIT,API_VERSION_URL } from "./constants.js";
 import cookie_parser from "cookie-parser";
-import {restrictToLoggedUserOnly} from './middleware/auth.middleware.js'
+import {requireAuth ,AllowTo} from './middleware/auth.middleware.js'
 
 const app = express();
 
@@ -18,6 +18,7 @@ app.use(express.json({ limit: DATA_LIMIT }));
 app.use(express.urlencoded({ extended: true, limit: DATA_LIMIT }));
 app.use(express.static("public"));
 app.use(cookie_parser(process.env.COOKIE_PARSER_SECRET));
+app.use(requireAuth)
 
 //Import routes
 import userRouter from './routes/user.router.js';
@@ -25,7 +26,7 @@ import todoRouter from './routes/todo.router.js';
 import subTodoRouter from './routes/subTodo.router.js';
 
 app.use(`${API_VERSION_URL}/users`,userRouter);
-app.use(`${API_VERSION_URL}/todo`,restrictToLoggedUserOnly,todoRouter);
-app.use(`${API_VERSION_URL}/subTodo`,restrictToLoggedUserOnly,subTodoRouter);
+app.use(`${API_VERSION_URL}/todo`,AllowTo(['ADMIN','USER']),todoRouter);
+app.use(`${API_VERSION_URL}/subTodo`,AllowTo(['ADMIN','USER']),subTodoRouter);
 
 export { app };
