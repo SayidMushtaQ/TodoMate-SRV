b427e2535e1ea258e573876649d24744325b580b
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-15 19:15:20 +0530

    IMP

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ import { UserTokenHandler } from "../util/authUserTokenHandler.js";
 import { ApiError } from "../util/apiError.js";
 import {excludedPaths} from '../constants.js'
 
-export const requireAuth  = asyncHanlder(async (req, _, next) => {
+export const requireAuth  = asyncHanlder(async (req, _, next) => { //Authenticaiton
   if(excludedPaths.includes(req.path)) return next(); //Exclude for: Login and Register
   const userToken = new UserTokenHandler();
   const token = req.cookies?.loginToken;
@@ -17,7 +17,7 @@ export const requireAuth  = asyncHanlder(async (req, _, next) => {
   return next();
 });
 
-export function AllowTo(roles = []) {
+export function AllowTo(roles = []) { //Authorization
   return asyncHanlder((req, res, next)=>{
     if (!req.user)  throw new ApiError(401, "Access denied. Please log in to continue.",['Unauthorized']);
 
