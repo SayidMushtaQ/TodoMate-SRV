57d873900c094a10f381c67fe6cba33597b58e60
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-23 17:16:30 +0530

    Imp

--- src/config/passport-setup.config.js
+++ src/config/passport-setup.config.js
@@ -1,17 +1,19 @@
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { GOOGLE_AUTH_REDIRECT_URL } from "../constants.js";
-import { User } from "../modules/user.model.js";
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { GOOGLE_AUTH_REDIRECT_URL } from '../constants.js';
+import { User } from '../modules/user.model.js';
 
 passport.serializeUser((user, done) => {
-  console.log(user.id)
   done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user.id);
-});
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 passport.use(
@@ -22,25 +24,31 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     },
     async (accessToken, refreshToken, profile, done) => {
-      const email = profile.emails[0].value;
-      const userName = email.split("@")[0];
-      const isVerified = profile.emails[0].verified;
-      const provider = profile.provider;
-      const googleID = profile.id;
-      const userExist = await User.findOne({ email });
+      try {
+        const email = profile.emails[0].value;
+        const userName = email.split('@')[0];
+        const isVerified = profile.emails[0].verified;
+        const provider = profile.provider;
+        const googleID = profile.id;
+        const userExist = await User.findOne({ email });
 
-      if (userExist) {
-        done(null, userExist);
-      } else {
-        const newUser = await User.create({
-          email,
-          userName,
-          provider,
-          isVerified,
-          googleID
-        });
-        done(null, newUser);
+        if (userExist) {
+          return done(null, userExist);
+        } else {
+          const newUser = await User.create({
+            email,
+            userName,
+            provider,
+            isVerified,
+            googleID
+          });
+          return done(null, newUser);
+        }
+      } catch (err) {
+        return done(err, null);
       }
     }
   )
 );
+
+export default passport;
