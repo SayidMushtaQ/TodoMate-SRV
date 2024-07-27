917aca23e4d279f6efb0698b22c3561769f776a3
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-27 17:50:11 +0530

    IMp JWT Verify Code

--- src/middleware/strategies/JWTcallbackHandler.js
+++ src/middleware/strategies/JWTcallbackHandler.js
@@ -1,13 +1,16 @@
 import { User } from "../../modules/user.model.js";
+import { ApiError } from "../../util/apiError.js";
 
-export const jwtCallbackHandler = (jwt_payload, cb) => {
-    User.findById(jwt_payload.id)
-      .then(user => {
-        if (user) {
-          return cb(null, user);
-        } else {
-          return cb(null, false);
-        }
-      })
-      .catch(err => console.error(err));
+export const jwtCallbackHandler = async (jwt_payload, cb) => {
+  try {
+    console.log("Run JWT")
+    const user = await User.findById(jwt_payload.id);
+    if (!user) {
+      return cb("Access denied due to invalid credentials[Unauthorized]", false);
+    }
+    return cb(null, user);
+  } catch (err) {
+    console.log(err);
+    throw new ApiError(500, "Something went wrong During JWT Verification", [err]);
   }
+};
