fdb14978c916d57d8f988ffe48c94a95bc7340d1
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-28 13:13:41 +0530

    Only ADMIN

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import { getAllUsers } from '../controllers/Admin/getAllUser.controller.js';
+import express from "express";
+import { getAllUsers } from "../controllers/Admin/getAllUser.controller.js";
+import { AllowTo } from "../middleware/auth.middleware.js";
+const router = express.Router();
 
-const router = express.Router(); 
-
-router.route('/getUsers').get(getAllUsers);
-
+router.route("/getUsers").get(AllowTo(["ADMIN"]), getAllUsers);
 
 export default router;
