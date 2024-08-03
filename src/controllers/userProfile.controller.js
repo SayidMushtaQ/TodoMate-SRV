4830ad576ea16bc5e6921b7ae9cb38e04083833c
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-03 16:48:35 +0530

    User Data Fetch - Controller

--- src/controllers/userProfile.controller.js
+++ src/controllers/userProfile.controller.js
@@ -1,5 +1,14 @@
 import { asyncHanlder } from "../util/asyncHandler.js";
-
+import {ApiResponse} from '../util/apiResponse.js'
 export const userProfile = asyncHanlder(async (req, res) => {
   res.send(`Welcome Back: ${req.user.userName} email: ${req.user.email} provider: ${req.user.provider}`);
+  const user = {
+    userNmae:req.user.userName,
+    email:req.user.email,
+    provider:req.user.provider,
+    role:req.user.role
+  }
+  res
+  .status(201)
+  .json(new ApiResponse(200, { user }, "Data fetched sucessfully"));
 });
