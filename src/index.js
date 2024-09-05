cd34ecf3528d5b7dc2d9e7f3e41f5217fbd6300f
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-05 19:56:35 +0530

    Add default PORT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {connectDB} from './config/connection.db.js'
 import {app} from './app.js';
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 
 connectDB().then(()=>{
